ceeaea818ea946005add00db7160a870c20137ad
olivrms <oliver.vermes@stud.uni-corvinus.hu>
2025-04-01 18:44:11 +0200

    Add files via upload

---
+++ MS SQL.1.sql
@@ -0,0 +1,64 @@
+/*1. Készítsünk nézetet VSZOBA néven, amely megjeleníti a szobák adatai mellett a megfelelő szálláshely nevét, helyét és a csillagok számát is!
+
+Az oszlopoknak nem szükséges külön nevet adni!
+Teszteljük is a nézetet, pl: SELECT * FROM VSZOBA*/
+CREATE OR Alter VIEW VSZOBA As 
+Select sz.*
+From szoba sz Join Szallashely szh On sz.SZALLAS_FK=szh.SZALLAS_ID
+Select * From VSZOBA
+
+/*2 Készítsen tárolt eljárást SPUgyfelFoglalasok, amely a paraméterként megkapott ügyfél azonosítóhoz tartozó foglalások adatait listázza!
+Teszteljük a tárolt eljárás működését, pl: EXEC SPUgyfelFoglalasok 'laszlo2'
+*/
+CREATE or alter procedure SPUgyfelFoglalasok
+@ugyfelazon nvarchar(40)
+As 
+BEGIN
+Select * 
+From Foglalas
+Where ugyfel_fk = @ugyfelazon
+End 
+EXEC SPUgyfelFoglalasok 'laszlo2'
+
+/*
+3. Készítsen skalár értékű függvényt UDFFerohely néven, amely visszaadja, hogy a paraméterként megkapott foglalás azonosítóhoz hány férőhelyes szoba tartozik!
+a. Teszteljük a függvény működését!
+*/
+
+CREATE Or ALTER Function UDFFerohely
+(
+	@fazon Int
+)
+RETURNS int 
+AS
+BEGIN
+DECLARE @ferohely INT
+RETURN
+(
+	SELECT @ferohely = sz.FEROHELY
+  	From Foglalas f Join Szoba sz On f.SZOBA_FK = sz.SZOBA_ID
+  	Where f.FOGLALAS_PK = @fazon
+)
+RETURN @ferohely
+END
+SELECT dbo.UDFFerohely(651)
+
+/*
+4. Készítsünk tárolt eljárást SPRangsor néven, amely rangsorolja a szálláshelyeket a foglalások száma alapján (a legtöbb foglalás legyen a rangsorban az első). A listában a szállás azonosítója, neve és a rangsor szerinti helyezés jelenjen meg - holtverseny esetén ugrással (ne sűrűn)! 
+a. Teszteljük a tárolt eljárást, pl: EXEC SPRangsor
+*/
+
+/*
+5. Készítsünk nézetet VFoglalasreszletek néven, amely a következő adatokat jeleníti meg: foglalás azonosítója, az ügyfél neve, a szálláshely neve és helye, a foglalás kezdete és vége, és a szoba száma. 
+
+a. Az oszlopokat nevezzük el értelemszerűen! 
+b. Teszteljük a nézet működését, pl: SELECT * FROM VFoglalasreszletek
+*/
+
+/*
+6.
+Készítsen tábla értékű függvényt NEPTUNKÓD_UDFFoglalasnelkuliek néven, amely azon ügyfelek adatait listázza, akik még nem foglaltak egyszer sem az adott évben adott hónapjában! A függvény paraméterként kapja meg a foglalás évét és hónapját! (Itt is a METTOL dátummal dolgozzunk) 
+a. Teszteljük is a függvény működését, pl: SELECT * FROM dbo.UJAENB_UDFFoglalasnelkuliek(2016, 10)
+*/
+
+
